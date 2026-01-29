55ec3e614688ee0d4f80b9dc3c55de6e0b9c648f
venkatesh58285 <venkyrachamadugu169@gmail.com>
2026-01-29 21:00:32 +0530

    Enables-cors in app

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,18 @@ require("./jobs/dueChecker.job");
 const app = express();
 app.use(express.json());
 
+// Allow CORS from frontend (Vite dev server)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  );
+  if (req.method === "OPTIONS") return res.sendStatus(204);
+  next();
+});
+
 app.use("/admin", require("./routes/admin.routes"));
 
 module.exports = app;
